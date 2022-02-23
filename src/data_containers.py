49bc36e680980af3799a28a838569c4305d7aaef
Maxim-Mushizky <maximmu87@gmail.com>
2022-02-23 09:40:15 +0200

    adding data for initial commit

---
+++ src/data_containers.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass,field
+from typing import (
+    Optional,
+    Any,
+    Dict
+)
+
+@dataclass
+class TestData:
+    test_input: Optional[Any] = field(default=None)
+    func_args: Optional[Dict] = field(default_factory=dict)
+    expected: Optional[Any] = field(default=None)
+    actual: Optional[Any] = field(default=None)
+    operator: Optional[str] = field(default=None)
+    test_func: Optional[str] = field(default=None)
+    test_status: int = field(default=0) # 1- Pass , 0- Fail, -1- Skip
+    call_duration: float = field(default_factory=float)
+    setup_duration: float = field(default_factory=float)
