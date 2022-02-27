9eb986e39bf0bfb8a6c9646876a0c7f4223e916e
ADAMZ <adamzaft@gmail.com>
2022-02-27 18:09:49 +0200

    Updated names: expected -> expected_result actual -> actual_result operator -> test_operator call_duration -> test_duration

--- pytest_data_extractor/data_containers.py
+++ pytest_data_extractor/data_containers.py
@@ -9,10 +9,10 @@ from typing import (
 class TestData:
     test_input: Optional[Any] = field(default=None)
     func_args: Optional[Dict] = field(default_factory=dict)
-    expected: Optional[Any] = field(default=None)
-    actual: Optional[Any] = field(default=None)
-    operator: Optional[str] = field(default=None)
+    expected_result: Optional[Any] = field(default=None)
+    actual_result: Optional[Any] = field(default=None)
+    test_operator: Optional[str] = field(default=None)
     test_func: Optional[str] = field(default=None)
     test_status: int = field(default=0) # 1- Pass , 0- Fail, -1- Skip
-    call_duration: float = field(default_factory=float)
+    test_duration: float = field(default_factory=float)
     setup_duration: float = field(default_factory=float)
