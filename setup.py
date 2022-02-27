028cde19e9b70785a43984264cd5d8224ecde567
Maxim-Mushizky <maximmu87@gmail.com>
2022-02-27 13:14:04 +0200

    removed comments from setup

--- setup.py
+++ setup.py
@@ -1,36 +1,3 @@
-# sample ./setup.py file
-# from setuptools import setup, find_packages
-# from pkg_resources import parse_requirements
-# import pathlib
-# import codecs
-# import os 
-
-
-# with pathlib.Path("requirements.txt").open() as req_txt:
-#     install_req = [str(req) for req in parse_requirements(req_txt)]
-
-# def read(fname):
-#     file_path = os.path.join(os.path.dirname(__file__), fname)
-#     return codecs.open(file_path, encoding='utf-8').read()
-
-
-# setup(
-#     name="pytest_data_extractor",
-#     version= '0.1.0',
-#     description = 'A pytest plugin to extract relevant metadata about tests into an external file (currently only json support)',
-#     author= 'Maxim Mushizky',
-#     author_email= 'maximmu87@gmail.com',
-#     license='MIT',
-#     package_dir='src',
-#     packages=['data_extractor'],
-#     # the following makes a plugin available to pytest
-#     entry_points={"pytest11": ["pytest_data_extractor = data_extractor.pytest_extractor"]},
-#     long_description=read('README.md'),
-#     python_requires='>=3.7',
-#     install_requires=install_req,
-#     # custom PyPI classifier for pytest plugins
-#     classifiers=["Framework :: Pytest"]
-# )
 import os
 import codecs
 from setuptools import setup
