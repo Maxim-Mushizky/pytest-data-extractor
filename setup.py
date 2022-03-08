1e97b5092b893dab69b2b17c715ad59acceacc4b
Maxim-Mushizky <maximmu87@gmail.com>
2022-03-08 16:18:44 +0200

    # 0.1.5 ## Added * Better management of pass/fail of tests with pytest_report_teststatus. * added .github folder with various workflows
    ## Changed
    * Fixed bug of failing passing tests that use exception with pytest.raises context manager.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="pytest_data_extractor",
-    version="0.1.4",
+    version="0.1.5",
     author="Maxim Mushizky",
     author_email="maximmu87@gmail.com",
     maintainer="Maxim Mushizky",
