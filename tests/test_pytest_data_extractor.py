76cfdd426ac67a0ad25fc9cdf59aad4c3210bbaf
MaximMu-CISEMI <55351322+MaximMu-CISEMI@users.noreply.github.com>
2022-07-19 21:20:58 +0300

    # 0.1.7 ## Added - additional test for intentional failure
    ## Changed
    - Saved json schema for tests.
    - All tests now will be stored in external files in the cache folder.
    - README.md was updated.
    - The data will be displayed for failed tests by default

--- tests/test_pytest_data_extractor.py
+++ tests/test_pytest_data_extractor.py
@@ -1,2 +1,3 @@
-def data_extractor_functionality():
-    assert True
+def test_data_extractor_functionality(upload_manager):
+    my_list = upload_manager([1,2,3])
+    assert isinstance(my_list,list)
